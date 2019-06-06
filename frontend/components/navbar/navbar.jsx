5ce917708d4582727482210d3351a839ae8ed15d
Simcha Cohen <simcha_cohen@yahoo.com>
2019-06-06 19:57:11 -0400

    add navbar and style

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signup, login } from '../../actions/session_actions';
 
-const Navbar = function(props) {
-  return (
-    <nav className="navbar">
-      <span className="logo">LOGO</span>
-      <div className="session-options">
-        <button>Sign Up</button>
-        <button>Log In</button>
-      </div>
-    </nav>
-  )
-};
+class Navbar extends React.Component {
 
-export default Navbar;
+  render () {
+    return (
+      <nav className="navbar">
+        <span className="logo">LOGO</span>
+        <div>
+          <button className="session-option-buttons">Sign Up</button>
+          <button className="session-option-buttons">Log In</button>
+        </div>
+      </nav>
+    )
+  }
+}
+
+const msp = (state) => {
+  return {
+    current_user: state.entities.users[state.session.currentUserId],
+  }
+}
+
+const mdp = (dispatch) => {
+  return {
+    // signup: (user) => dispatch(signup(user)),
+    // login: (user) => dispatch(login(user)),
+  }
+}
+
+
+
+export default connect(msp, mdp)(Navbar);
