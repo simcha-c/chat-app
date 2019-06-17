3e8146d57c23db4197f892dfa45eefe351a7e588
Simcha Cohen <simcha_cohen@yahoo.com>
2019-06-17 09:59:33 -0400

    clear errors when close modal

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,28 +1,29 @@
 import { CLEAR_ERRORS, RECEIVE_ERRORS } from '../actions/error_actions';
+import { CLOSE_MODAL } from '../actions/modal_actions';
 
 const defaultState = {
     type: null,
     errors: []
-}
+};
 
 const errorsReducer = (state = defaultState, action) => {
-    Object.freeze(state);
-    
-    switch (action.type) {
-        case RECEIVE_ERRORS:
-            const newState = {
-                type: action.errorType,
-                errors: action.errors,
-            };
-            return newState;
+  Object.freeze(state);
+  
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      const newState = {
+        type: action.errorType,
+        errors: action.errors,
+      };
+      return newState;
 
-        case CLEAR_ERRORS:
-            return defaultState;
+    case CLEAR_ERRORS:
+    case CLOSE_MODAL:
+      return defaultState;
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
+};
 
-}
-
-export default errorsReducer
+export default errorsReducer;
