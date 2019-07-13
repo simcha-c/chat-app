ab034a63f9de0d6fdf087aced340c8e6d97403b4
Alan Uraz <alan@AlansMBP.local>
2019-07-12 21:22:52 -0400

    Add CSS to switch session component

--- frontend/components/session/switch_session.jsx
+++ frontend/components/session/switch_session.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const SwitchSession = ({props}) => {
-  const otherModal = props.modal === 'SIGN UP' ? 'login' : 'signup';
+  let otherModal;
+  let text;
+  let buttonText;
+
+  if (props.modal === 'SIGN UP') {
+    otherModal = 'login';
+    text = 'Already a user?'
+    buttonText = 'Log In';
+  } else {
+    otherModal = 'signup';
+    text = 'Don\'t have an account?';
+    buttonText = 'Sign Up';
+  }
+
   return (
     <React.Fragment>
-      <h3 onClick={() => props.openModal(otherModal)}>CLICK ME</h3>
+      <h3 className='switch-session-text'>{text}</h3>
+      <h3 className='switch-session-button' type='submit' onClick={() => props.openModal(otherModal)}>{buttonText}</h3>
     </React.Fragment>
   )
-
 }
 
 export default SwitchSession;
