5f1de75b76d6d0e311e9d97cc70dff8fd0b9998f
Simcha Cohen <simcha_cohen@yahoo.com>
2019-07-30 20:25:00 -0400

    Add chat groups section

--- frontend/components/chat/chat.jsx
+++ frontend/components/chat/chat.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ChatMessages from './chat_messages/chat_messages';
+import ChatGroups from './chat_groups/chat_groups';
 
 function Chat(props) {
   return (
     <section className="chat-section">
-
+      <ChatGroups />
       <ChatMessages />
     </section>
   )
---
+++ frontend/components/chat/chat_groups/chat_groups.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatGroups(props) {
+  return (
+    <section className="chat-groups-section">
+      <h1>Hello from groups</h1>
+    </section>
+  )
+}
+
+export default ChatGroups;
